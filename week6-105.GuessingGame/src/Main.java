0b2cbeb1215ee942a7c58ff02c7ab61730208236
alinaalam <alinaalamzeb@gmail.com>
2019-03-26 22:48:40 +0100

    fixed second assignment and worked on more exercises

--- week6-105.GuessingGame/src/Main.java
+++ week6-105.GuessingGame/src/Main.java
@@ -3,6 +3,8 @@ public class Main {
     public static void main(String[] args) {
         // test your program here
         GuessingGame game = new GuessingGame();
-        game.play(1,10);
+        game.play(1,40);
+        System.out.println(game.isGreaterThan(32));
+        System.out.println(game.average(3, 4));
     }    
 }
